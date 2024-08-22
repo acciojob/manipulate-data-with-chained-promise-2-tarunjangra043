a10fe3647e97b7dfd4f7826e7df6c738a8a254af
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-22 20:25:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+function getNumbers() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+function filterEvenNumbers(numbers) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const evenNumbers = numbers.filter(num => num % 2 === 0);
+            document.getElementById('output').innerText = evenNumbers.join(', ');
+            resolve(evenNumbers);
+        }, 1000);
+    });
+}
+
+function multiplyNumbers(numbers) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const multipliedNumbers = numbers.map(num => num * 2);
+            document.getElementById('output').innerText = multipliedNumbers.join(', ');
+            resolve(multipliedNumbers);
+        }, 2000);
+    });
+}
+
+getNumbers()
+    .then(filterEvenNumbers)
+    .then(multiplyNumbers)
+    .catch(error => {
+        console.error('Error:', error);
+    });
